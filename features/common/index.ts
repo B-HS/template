e7e67dcf09d2f90e056dfc8539dd4728b4606d6c
Hyunseok Byun <gumyoincirno@gmail.com>
2024-07-28 12:55:00 +0900

    Update type and styles for go to top component

--- features/common/index.ts
+++ features/common/index.ts
@@ -1,4 +1,4 @@
-export { GoToTop } from './go-to-tap'
+export { GoToTop } from './go-to-top'
 export { ScrollStatus } from './scroll-status'
 export { ThemeChanger } from './theme-changer'
 export { TooltipIcon } from './tooltip-icon'
