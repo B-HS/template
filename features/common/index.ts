c81f89da0b39a7aa72dd6d7505dfb2527d79608e
Hyunseok Byun <gumyoincirno@gmail.com>
2024-07-28 12:56:33 +0900

    Add rating component

--- features/common/index.ts
+++ features/common/index.ts
@@ -1,4 +1,5 @@
 export { GoToTop } from './go-to-top'
 export { ScrollStatus } from './scroll-status'
+export { StarRating } from './star-rating'
 export { ThemeChanger } from './theme-changer'
 export { TooltipIcon } from './tooltip-icon'
