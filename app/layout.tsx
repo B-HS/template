a6f870b4fcc1f88e11f44112a89e7d4b6dbaa7a6
Hyunseok Byun <gumyoincirno@gmail.com>
2024-09-13 19:53:42 +0900

    Fix styling for body, html

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ const RootLayout: FC<{ children: ReactNode }> = ({ children }) => {
                 <SessionProvider>
                     <ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
                         <SiteHeader />
-                        <section className='max-w-screen-2xl mx-auto overflow-auto h-full w-full'>
+                        <section className='max-w-screen-2xl mx-auto overflow-auto size-full flex-1'>
                             {children}
                             <GoToTop />
                         </section>
