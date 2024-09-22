2800f5e5f5c186b45465d7ad2bac27b712c08773
Hyunseok Byun <gumyoincirno@gmail.com>
2024-09-22 16:36:35 +0900

    Fix setting for deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
+import { env } from '@cloudflare/next-on-pages'
 import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    env,
     transpilePackages: ['shiki', 'next-mdx-remote', 'next-mdx-remote/serialize'],
     images: {
         formats: ['image/avif', 'image/webp'],
@@ -14,5 +16,6 @@ const nextConfig = {
     },
 }
 process.env.NODE_ENV === 'development' && (await setupDevPlatform())
+console.log(ebv)
 
 export default nextConfig
