2692b5a76da501570a1fc70caf64b9f8ae3dc38a
Hyunseok Byun <gumyoincirno@gmail.com>
2024-09-22 16:18:35 +0900

    Update enviroment setting

--- generate-env.js
+++ generate-env.js
@@ -1,18 +1,16 @@
 const fs = require('fs')
 
-const requiredEnvVars = ['AUTHOR', 'COGNITO_CLIENT_ID', 'COGNITO_USER_POOL_ID', 'NEXTAUTH_SECRET', 'SITE_NAME']
+const variables = ['AUTHOR', 'COGNITO_CLIENT_ID', 'COGNITO_USER_POOL_ID', 'NEXTAUTH_SECRET', 'SITE_NAME']
+variables.forEach((variable) => {
+    const value = process.env[variable]
+    if (value) {
+        console.log(`Variable ${variable}: ${value}`)
+    } else {
+        console.warn(`Warning: Environment variable ${variable} is not set.`)
+    }
+})
 
-const envContent = requiredEnvVars
-    .map((key) => {
-        const value = process.env[key]
-        console.log(`Variable ${key}:`, value)
-        if (!value) {
-            console.warn(`Warning: Environment variable ${key} is not set.`)
-        }
-        return `${key}=${value || ''}`
-    })
-    .join('\n')
+const envContent = variables.map((variable) => `${variable}=${process.env[variable] || ''}`).join('\n')
 
-fs.writeFileSync('.env', envContent, 'utf8')
-
+fs.writeFileSync('.env', envContent)
 console.log('.env file has been generated successfully')
