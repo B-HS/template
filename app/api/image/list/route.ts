d6bbfd2ca24d2ce3275933bbbd2e893d2cedd846
Hyunseok Byun <gumyoincirno@gmail.com>
2025-04-06 00:03:34 +0900

    Update bucket name as using env variable

--- app/api/image/list/route.ts
+++ app/api/image/list/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from 'next/server'
 
 const imgList = async () => {
     const command = new ListObjectsCommand({
-        Bucket: 'blog',
+        Bucket: process.env.BUCKET_NAME,
     })
     const list = await R2Client.send(command)
     return list
